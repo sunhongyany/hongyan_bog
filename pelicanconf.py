9a83327fe61062941e09d617616580d8bb2160d6
sunhongyan <2960818158@qq.com>
2018-03-26 22:11:40 +0800

    保存数据

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,13 +3,17 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'sunhongyan'
-SITENAME = u'Stay hungry, stay foolish'
+SITE_DESCRIPTION = u'Stay hungry, stay foolish'
+SITENAME = u'梦与想'
 SITEURL = ''
 
+DEFAULT_PAGINATION = 3
 PATH = 'content'
 
+STATIC_PATHS = ['images', 'pdfs']
+
 TIMEZONE = 'Asia/Shanghai'
-THEME = "notmyidea-cms-fr"
+THEME = "maupassant-pelican"
 
 DEFAULT_LANG = u'zh'
 
@@ -24,7 +28,16 @@ AUTHOR_FEED_RSS = None
 
 # Social widget
 
-DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+MENUITEMS = [
+    ('首页', '/', 'fa-home'),
+    ('文章归档', '/archives.html', 'fa-archive'),
+]
+
+# 插件目录
+PLUGIN_PATHS = [u"pelican-plugins"]
+
+YEAR_ARCHIVE_SAVE_AS = 'pages/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'pages/{date:%Y}/{date:%b}/index.html'
